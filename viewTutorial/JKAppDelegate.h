97ccfea47ceb7b4bd2e2a41c4a4f2bc87f681f39
Guest User <Guest@terabithia.cs.hmc.edu>
2014-09-10 18:04:46 -0700

    Initial Commit

---
+++ viewTutorial/JKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKAppDelegate.h
+//  viewTutorial
+//
+//  Created by Guest User on 9/10/14.
+//  Copyright (c) 2014 Josh Kutsko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ viewTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  viewTutorial
+//
+//  Created by Guest User on 9/10/14.
+//  Copyright (c) 2014 Josh Kutsko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JKAppDelegate class]));
+    }
+}
